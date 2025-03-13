49587203db9cacee62b067a470fd8804ddbc49f6
akash172891 <76027562+akash172891@users.noreply.github.com>
2025-03-13 10:22:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    // Function to return max value that can be put in knapsack of capacity.
+    int knapsack(int capacity, vector<int> &val, vector<int> &wt) {
+        // code here
+        vector<int>dp(capacity+1,0);
+        for(int i=1;i<=val.size();i++)
+        for(int j=capacity;j>=1;j--){
+            if(wt[i-1]<=j)
+            dp[j]=max(val[i-1]+dp[j-wt[i-1]],dp[j]);
+        }
+        return dp[capacity];
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    // Taking total test cases
+    int testCases;
+    cin >> testCases;
+    cin.ignore();
+    while (testCases--) {
+        // Reading number of items and capacity
+        int numberOfItems, capacity;
+        vector<int> weights, values;
+        string input;
+        int number;
+
+        // Read capacity and number of items
+        getline(cin, input);
+        stringstream ss(input);
+        ss >> capacity;      // The first number is the capacity
+        ss >> numberOfItems; // The second number is the number of items
+
+        // Read values
+        getline(cin, input);
+        ss.clear();
+        ss.str(input);
+        while (ss >> number) {
+            values.push_back(number);
+        }
+
+        // Read weights
+        getline(cin, input);
+        ss.clear();
+        ss.str(input);
+        while (ss >> number) {
+            weights.push_back(number);
+        }
+
+        Solution solution;
+        cout << solution.knapsack(capacity, values, weights) << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
