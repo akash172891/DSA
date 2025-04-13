bed24624e3df2345106c7ad896aef659b25808e2
akash172891 <76027562+akash172891@users.noreply.github.com>
2025-04-13 19:26:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Clone an Undirected Graph/clone-an-undirected-graph.cpp
+++ Difficulty: Medium/Clone an Undirected Graph/clone-an-undirected-graph.cpp
@@ -1,5 +1,5 @@
 //{ Driver Code Starts
-//Initial Template for C++
+// Initial Template for C++
 
 #include <bits/stdc++.h>
 #include <sstream>
@@ -8,33 +8,37 @@ using namespace std;
 struct Node {
     int val;
     vector<Node*> neighbors;
+
     Node() {
         val = 0;
         neighbors = vector<Node*>();
     }
+
     Node(int _val) {
         val = _val;
         neighbors = vector<Node*>();
     }
+
     Node(int _val, vector<Node*> _neighbors) {
         val = _val;
         neighbors = _neighbors;
     }
 };
-vector<Node*> bfs(Node *src){
-    vector<Node*>ans;
+
+vector<Node*> bfs(Node* src) {
+    vector<Node*> ans;
     map<Node*, bool> visit;
     queue<Node*> q;
     q.push(src);
     visit[src] = true;
     while (!q.empty()) {
-        Node *u = q.front();
+        Node* u = q.front();
         ans.push_back(u);
         q.pop();
-        vector<Node *> v = u->neighbors;
+        vector<Node*> v = u->neighbors;
         int n = v.size();
-        for (int i = 0; i < n; i++){
-            if (!visit[v[i]]){
+        for (int i = 0; i < n; i++) {
+            if (!visit[v[i]]) {
                 visit[v[i]] = true;
                 q.push(v[i]);
             }
@@ -43,7 +47,8 @@ vector<Node*> bfs(Node *src){
     return ans;
 }
 
-bool compare(Node* prev, Node* new_node, unordered_set<Node*>& prev_vis, unordered_set<Node*>& new_vis) {
+bool compare(Node* prev, Node* new_node, unordered_set<Node*>& prev_vis,
+             unordered_set<Node*>& new_vis) {
     if (prev == new_node) {
         return false;
     }
@@ -55,7 +60,8 @@ bool compare(Node* prev, Node* new_node, unordered_set<Node*>& prev_vis, unorder
     }
 
     if (prev_vis.count(prev) || new_vis.count(new_node)) {
-        if ((prev_vis.count(prev) && !new_vis.count(new_node)) || (!prev_vis.count(prev) && new_vis.count(new_node))) {
+        if ((prev_vis.count(prev) && !new_vis.count(new_node)) ||
+            (!prev_vis.count(prev) && new_vis.count(new_node))) {
             return false;
         }
         return true;
@@ -73,8 +79,10 @@ bool compare(Node* prev, Node* new_node, unordered_set<Node*>& prev_vis, unorder
         return false;
     }
 
-    sort(prev->neighbors.begin(), prev->neighbors.end(), [](Node* a, Node* b) { return a->val < b->val; });
-    sort(new_node->neighbors.begin(), new_node->neighbors.end(), [](Node* a, Node* b) { return a->val < b->val; });
+    sort(prev->neighbors.begin(), prev->neighbors.end(),
+         [](Node* a, Node* b) { return a->val < b->val; });
+    sort(new_node->neighbors.begin(), new_node->neighbors.end(),
+         [](Node* a, Node* b) { return a->val < b->val; });
 
     for (size_t i = 0; i < prev_n; ++i) {
         if (!compare(prev->neighbors[i], new_node->neighbors[i], prev_vis, new_vis)) {
@@ -88,7 +96,7 @@ bool compare(Node* prev, Node* new_node, unordered_set<Node*>& prev_vis, unorder
 
 // } Driver Code Ends
 
-//User function Template for C++
+// User function Template for C++
 
 // struct Node {
 //     int val;
@@ -108,8 +116,8 @@ bool compare(Node* prev, Node* new_node, unordered_set<Node*>& prev_vis, unorder
 // };
 
 class Solution {
-public:
-    unordered_map<Node*,Node*> um;
+  public:
+     unordered_map<Node*,Node*> um;
     Node* cloneGraph(Node* node) {
         if(!node)
             return(node);
@@ -138,10 +146,11 @@ int main() {
         int N;
         cin >> N;
         Node* root = NULL;
-        vector<Node*>v(N);
+        vector<Node*> v(N);
         std::string buffer;
         std::getline(std::cin, buffer);
-        for (int i = 0; i < N; i++)v[i] = new Node(i);
+        for (int i = 0; i < N; i++)
+            v[i] = new Node(i);
         for (int i = 0; i < N; i++) {
             std::vector<Node*> vec;
             std::string buffer;
@@ -153,15 +162,18 @@ int main() {
             v[i]->neighbors = vec;
         }
         Solution ob;
-        vector<Node*>prev = bfs(v[0]);
+        vector<Node*> prev = bfs(v[0]);
         Node* ans = ob.cloneGraph(v[0]);
-        //vector<Node*>now = bfs(ans);
-        unordered_set<Node*>prev_vis, new_vis;
-        cout << compare(v[0], ans, prev_vis, new_vis) << endl;
+        // vector<Node*>now = bfs(ans);
+        unordered_set<Node*> prev_vis, new_vis;
+        if (compare(v[0], ans, prev_vis, new_vis))
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
 
-    
-cout << "~" << "\n";
-}
+        cout << "~"
+             << "\n";
+    }
     return 0;
 }
 // } Driver Code Ends
