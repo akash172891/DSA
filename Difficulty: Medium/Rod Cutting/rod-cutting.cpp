8c3461ca4fda37e6720434f906d4aba11bf70ab4
akash172891 <76027562+akash172891@users.noreply.github.com>
2025-05-22 14:52:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rod Cutting/rod-cutting.cpp
+++ Difficulty: Medium/Rod Cutting/rod-cutting.cpp
@@ -1,92 +1,23 @@
-//{ Driver Code Starts
-// Initial Template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 // User function Template for C++
 
 class Solution {
   public:
-  
-    int fxn(vector<int> arr, int ind, int k, vector<vector<int>> &dp) {
-        if(ind < 0 || k < 0) return 0;
-        if(dp[ind][k]!=0) return dp[ind][k];
-        
-        int take =0, notake =0;
-        int length = ind+1;
-        
-        if(k>=length) {
-            take = arr[ind] + fxn(arr, ind, k-length, dp);
-        }
-        
-        notake = fxn(arr, ind-1, k,dp);
-        
-        return dp[ind][k]=max(take, notake);
-    }
+  int fxn(vector<int>& arr, int ind, int sum, vector<vector<int>>&dp) {
+      if(sum<0 || ind<0) return 0;
+      if(dp[ind][sum]!=-1) return dp[ind][sum];
+      
+      int take = 0, notake = 0;
+      if(sum > ind)
+      take = arr[ind] + fxn(arr, ind, sum-ind-1, dp);
+      notake = 0+ fxn(arr, ind-1, sum, dp);
+      
+      return dp[ind][sum] = max(take, notake);
+  }
     int cutRod(vector<int> &price) {
         // code here
         int n = price.size();
-        
-        int k = n;
-        vector<vector<int>> dp(n+1 ,vector<int>(n+1, 0));
-        for(int i=0;i<n+1;i++) {
-            dp[i][0] = 0;
-            dp[0][i] =0;
-            
-            
-        }
-        
-        
-        for(int ind=1;ind<=n;ind++) {
-            for(int k = 1;k<=n;k++) {
-                int take =0, notake =0;
-                int length = ind;
-                
-                if(k>=length) {
-                    take = price[ind-1] + dp[ind][k-length];
-                }
-                
-                notake = dp[ind-1][k];
-                
-                dp[ind][k]=max(take, notake);
-            }
-        }
-        
-        
-        
-        
-        return dp[n][n];
-        
+        vector<vector<int>> dp(n, vector<int>(n+1, -1));
+        return fxn(price, n-1, n,dp);
         
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    scanf("%d ", &t);
-    while (t--) {
-
-        vector<int> a;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            a.push_back(number);
-        }
-
-        Solution ob;
-
-        cout << ob.cutRod(a) << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
